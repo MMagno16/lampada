a15e64c781ad6d18d997f5d6cda8d7024e20d543
Marianna Magno <mmagnosorio@gmail.com>
2022-03-01 09:49:00 -0300

    Projeto Lâmpada

--- lampada.js
+++ lampada.js
@@ -1,30 +1,30 @@
-const turnOn = document.getElementById (' turnOn');
-const turnOff = document.getElementById (' turnOff');
-const lamp = document.getElementById (' lamp ');
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
 
-function isLampBroken () {
-    return lamp.src.indexOf('opção1') > -1;
+function isLampBroken() {
+    return lamp.src.indexOf('moana') > -1;
 }
 
-function lampOn () {
-    if (!isLampBroken() ) {
-        lamp.src ="./img/acesa.png";
+function lampOn() {
+    if (!isLampBroken()) {
+        lamp.src = "./img/ligada.jpg";
     }
 }
 
-function lampOff () {
-    if (!isLampBroken() ) {
-    lamp.src="./img/desligada.png";
+function lampOff() {
+    if (!isLampBroken()) {
+        lamp.src = "./img/desligada.jpg";
     }
 }
 
-function lampBroken () {
-    lamp.src="./img/opção1.png";
+function lampBroken() {
+    lamp.src = "./img/moana.jpeg";
 }
 
 
-turnOn.addEventListener ('click', lampOn );
-turnOff.addEventListener ('click', lampOff);
-lamp.addEventListener ('mouseover', lampOn );
-lamp.addEventListener ('mouseleave', lampOff);
-lamp.addEventListener ('dblclick', lampBroken );
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+// lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
